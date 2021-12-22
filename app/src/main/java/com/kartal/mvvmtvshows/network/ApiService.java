059f1d3c2bdd637655b68bf395c6df3cc79187ee
kartalyusuf <yusufkartall97@hotmail.com>
2021-12-22 18:35:30 +0300

    Add ApiService file

--- app/src/main/java/com/kartal/mvvmtvshows/network/ApiService.java
+++ app/src/main/java/com/kartal/mvvmtvshows/network/ApiService.java
@@ -1,4 +1,15 @@
 package com.kartal.mvvmtvshows.network;
 
+import com.kartal.mvvmtvshows.responses.TVShowsResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface ApiService {
+
+    @GET("most-popular")
+    Call<TVShowsResponse> getMostPopularTVShows(@Query("page") int page) ;
+
+    
 }
