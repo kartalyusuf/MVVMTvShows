db8530b9e223420364465a9359be3013a3c225bc
kartalyusuf <yusufkartall97@hotmail.com>
2021-12-23 02:46:13 +0300

    Add to Files

--- app/src/main/java/com/kartal/mvvmtvshows/activities/MainActivity.java
+++ app/src/main/java/com/kartal/mvvmtvshows/activities/MainActivity.java
@@ -1,16 +1,31 @@
 package com.kartal.mvvmtvshows.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.kartal.mvvmtvshows.R;
+import com.kartal.mvvmtvshows.viewmodels.MostPopularTVShowsViewModel;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MostPopularTVShowsViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewModel = new ViewModelProvider(this).get(MostPopularTVShowsViewModel.class);
+        getMostPopularTVShows();
+
+    }
+
+    private void getMostPopularTVShows() {
+        viewModel.getMostPopularTVShows(0).observe(this, mostPopularTVShowsResponse ->
+                Toast.makeText(getApplicationContext(), "Total Pages: "
+                        + mostPopularTVShowsResponse.getTotalPages(), Toast.LENGTH_SHORT).show());
+
     }
 }
--- app/src/main/java/com/kartal/mvvmtvshows/viewmodels/MostPopularTVShowsViewModel.java
+++ app/src/main/java/com/kartal/mvvmtvshows/viewmodels/MostPopularTVShowsViewModel.java
@@ -18,6 +18,6 @@ public class MostPopularTVShowsViewModel extends ViewModel {
     public LiveData<TVShowsResponse> getMostPopularTVShows(int page) {
         return mostPopularTVShowsRepository.getMostPopularTVShows(page);
 
-        
+
     }
 }
