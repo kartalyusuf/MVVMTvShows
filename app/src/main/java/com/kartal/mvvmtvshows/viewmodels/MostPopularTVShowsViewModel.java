e6f98462c3bbb9d84a80b06e0e534d733c31c053
kartalyusuf <yusufkartall97@hotmail.com>
2021-12-22 19:55:29 +0300

    Add to Files

---
+++ app/src/main/java/com/kartal/mvvmtvshows/viewmodels/MostPopularTVShowsViewModel.java
@@ -0,0 +1,23 @@
+package com.kartal.mvvmtvshows.viewmodels;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.kartal.mvvmtvshows.repositories.MostPopularTVShowsRepository;
+import com.kartal.mvvmtvshows.responses.TVShowsResponse;
+
+public class MostPopularTVShowsViewModel extends ViewModel {
+
+    private MostPopularTVShowsRepository mostPopularTVShowsRepository ;
+
+    public MostPopularTVShowsViewModel() {
+        mostPopularTVShowsRepository = new MostPopularTVShowsRepository();
+
+    }
+
+    public LiveData<TVShowsResponse> getMostPopularTVShows(int page) {
+        return mostPopularTVShowsRepository.getMostPopularTVShows(page);
+
+        
+    }
+}
